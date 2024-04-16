b86ec8eb485acacaa838e9364c97ee08628f823c
Venomkajo <kajetanrespondek@gmail.com>
2024-04-16 23:42:11 +0200

    refactored code to split into two functions

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 // weather API 61fe5866c8534094a4804224241604
 
-async function updateDisplay(){
-    // update today
+let data = '';
+let input = 'wroclaw';
+
+startApi();
+
+async function startApi(){
+    data = await getApi(input);
+    updateDisplay(data);
+}
+
+async function getApi(apiInput){
+    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=61fe5866c8534094a4804224241604&q=' + apiInput + '&days=3&aqi=no&alerts=no', {mode: "cors"});
+    const apiData = await response.json();
+    return apiData;
+}
+
+function updateDisplay(updateData) {
+    // update current
     const weatherImage = document.getElementById('weatherImage');
     const weatherDisplay = document.getElementById('weatherDisplay');
     const weatherTemperature = document.getElementById('weatherTemperature');
 
-    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=61fe5866c8534094a4804224241604&q=London&days=3&aqi=no&alerts=no', {mode: "cors"});
-    const data = await response.json();
-    weatherImage.src = data.current.condition.icon;
-    weatherDisplay.innerText = data.current.condition.text;
-    weatherTemperature.innerText = data.current.temp_c;
+    weatherImage.src = updateData.current.condition.icon;
+    weatherDisplay.innerText = updateData.current.condition.text;
+    weatherTemperature.innerText = updateData.current.temp_c + 'C';
 }
-
-updateDisplay();
 
 console.log('loading complete');
