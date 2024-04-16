5846b2b3c31ac9abb7e02284dc0d92cfebf8eafc
Venomkajo <kajetanrespondek@gmail.com>
2024-04-16 22:36:51 +0200

    basic API connection, basic layout

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// weather API 61fe5866c8534094a4804224241604
+
+async function updateDisplay(){
+    // update today
+    const weatherImage = document.getElementById('weatherImage');
+    const weatherDisplay = document.getElementById('weatherDisplay');
+    const weatherTemperature = document.getElementById('weatherTemperature');
+
+    const response = await fetch('https://api.weatherapi.com/v1/forecast.json?key=61fe5866c8534094a4804224241604&q=London&days=3&aqi=no&alerts=no', {mode: "cors"});
+    const data = await response.json();
+    weatherImage.src = data.current.condition.icon;
+    weatherDisplay.innerText = data.current.condition.text;
+    weatherTemperature.innerText = data.current.temp_c;
+}
+
+updateDisplay();
+
 console.log('loading complete');
